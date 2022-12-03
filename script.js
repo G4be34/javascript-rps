17da692420840b52388ef426057f702fddddefe0
G4be34 <jimenezgabriel12@gmail.com>
2022-12-02 21:48:31 -0700

    Add player and computer score variables

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let playerScore = 0
+let computerScore = 0
+let draws = 0
 
-prompt("Would you like to use Rock, Paper, or Scissors?")
+let playerInput = prompt("Would you like to use Rock, Paper, or Scissors?")
 
 function getComputerChoice(){
     const computerChoices = ['rock', 'paper', 'scissors'];
@@ -7,26 +10,35 @@ function getComputerChoice(){
    return computerChoices[randomChoice];
    }
 
-let playerSelection = 'Scissors'.toLowerCase()
+let playerSelection = playerInput.toLowerCase()
 let computerSelection = getComputerChoice()
 
+function game() {
+    let roundNumber = playRound()
+    for (i = 1; i < 5; i++) {
+        console.log(playRound(playerSelection, computerSelection))
+    }
+    
+}   
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return "You lose! Paper beats Rock"
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return "You lose! Scissors beats Paper"
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return "You lose! Rock beats Scissors"
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return "You win! Paper beats Rock"
+   if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return playerWinRound
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return "You win! Rock beats Scissors"
+        return playerWinRound
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return "You win! Scissors beats Paper"
+        return playerWinRound
     } else if (playerSelection == computerSelection) {
-        return "It\'s a tie! Try again"
+        return roundDraw
+    } else {
+        return computerWinRound
     }
 }
 
+let playerWinRound = "Player wins this round!"
+let computerWinRound = "Computer wins this round!"
+let roundDraw = "This round is a draw!"
+let playerWinGame = "Congrats! Player wins the game!"
+let computerWinGame = "Computer wins the game! Better luck next time!"
 
 
