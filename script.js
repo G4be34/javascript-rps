aa506e4794fc4b508a0a96c0b00434c6145e62dd
G4be34 <jimenezgabriel12@gmail.com>
2022-12-02 18:47:25 -0700

    Add if else statements for winning and losing conditions

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function getComputerChoice(){
    const randomChoice = Math.floor(Math.random()* computerChoices.length);
    return computerChoices[randomChoice];
    }
+
+const playerSelection = 'Paper'.toLowerCase;
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+        return "You lose! Paper beats Rock"
+    } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
+        return "You lose! Scissors beats Paper"
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
+        return "You lose! Rocks beats Scissors"
+    } else if (playerSelection == 'paper' && computerSelection == 'Rock') {
+        return "You win! Paper beats Rock"
+    } else if (playerSelection == 'rock' && computerSelection == 'Scissors') {
+        return "You win! Rock beats Scissors"
+    } else if (playerSelection == 'scissors' && computerSelection == 'Paper') {
+        return "You win! Scissors beats Paper"
+    }
+}
+
+
