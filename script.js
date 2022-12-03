8a0d4365fac0e7d4f252953e4a97a092a02cd88f
G4be34 <jimenezgabriel12@gmail.com>
2022-12-02 22:15:06 -0700

    Add for loop conditions and break

--- script.js
+++ script.js
@@ -2,24 +2,12 @@ let playerScore = 0
 let computerScore = 0
 let draws = 0
 
-let playerInput = prompt("Would you like to use Rock, Paper, or Scissors?")
 
 function getComputerChoice(){
     const computerChoices = ['rock', 'paper', 'scissors'];
    const randomChoice = Math.floor(Math.random()* computerChoices.length);
    return computerChoices[randomChoice];
    }
-
-let playerSelection = playerInput.toLowerCase()
-let computerSelection = getComputerChoice()
-
-function game() {
-    let roundNumber = playRound()
-    for (i = 1; i < 5; i++) {
-        console.log(playRound(playerSelection, computerSelection))
-    }
-    
-}   
 
 function playRound(playerSelection, computerSelection) {
    if (playerSelection == 'paper' && computerSelection == 'rock') {
@@ -40,5 +28,17 @@ let computerWinRound = "Computer wins this round!"
 let roundDraw = "This round is a draw!"
 let playerWinGame = "Congrats! Player wins the game!"
 let computerWinGame = "Computer wins the game! Better luck next time!"
+
+for (let i = 0; i < 100; i++) {
+    let playerInput = prompt("Would you like to use Rock, Paper, or Scissors?");
+    let playerSelection = playerInput.toLowerCase();
+    let computerSelection = getComputerChoice();
+    let roundResult = playRound(playerSelection, computerSelection);
+    console.log(roundResult);
+    gameScore(roundResult); 
+    if (playerScore === 5 || computerScore === 5) {
+        break;
+    }
+}
 
 
